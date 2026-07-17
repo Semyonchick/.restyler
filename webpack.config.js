744846335221a4c12e03b4e7e1ae6637b317d551
Semyonchick <semyonchick@gmail.com>
2026-07-17 09:34:54 +0500

    Remove deprecated build dependencies where compatible
    Modernize the Webpack 4 toolchain while preserving Vue 2 and existing project structure. Replace deprecated polyfills and plugins, remove unused loaders, update HTTPS dev-server configuration, refresh the npm 11 lockfile, and keep CI reproducible.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,81 @@
-module.exports = (env, argv) => {
+module.exports = (env = {}, argv = {}) => {
   const production = argv.mode === 'production';
   const analyze = argv.test === 'analyze';
+  const serving = Boolean(env.WEBPACK_SERVE);
 
   const config = require('./build.config.js');
   const packageJson = require('./package.json');
 
+  const fs = require('fs');
   const path = require('path');
   const webpack = require('webpack');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+  const {VueLoaderPlugin} = require('vue-loader');
+  const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+  const autoprefixer = require('autoprefixer');
+  const cssnano = require('cssnano');
+  const {simpleGit} = require('simple-git');
+
   const publicPath = (config.dir || './build') + '/';
   const resultPath = path.resolve(__dirname, '..' + publicPath);
-  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-  const CleanWebpackPlugin = require('clean-webpack-plugin');
-  const CopyWebpackPlugin = require('copy-webpack-plugin');
-  // const WebpackFtpUpload = require('webpack-ftp-upload-plugin')
-  const autoprefixer = require('autoprefixer');
-  const cssnano = require('cssnano')({zindex: false});
-  const {VueLoaderPlugin} = require('vue-loader');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const fs = require('fs');
   const port = require('portfinder-sync').getPort(8080);
+  const developmentUrl = 'https://localhost:' + port + '/';
 
-  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-  const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+  class StaticCopyPlugin {
+    constructor (sourceDirectory) {
+      this.sourceDirectory = sourceDirectory;
+    }
 
-  //MultiPage
+    apply (compiler) {
+      compiler.hooks.emit.tapAsync('StaticCopyPlugin', (compilation, callback) => {
+        const sourceRoot = path.resolve(__dirname, this.sourceDirectory);
+        if (!fs.existsSync(sourceRoot)) {
+          callback();
+          return;
+        }
+
+        const addDirectory = (directory) => {
+          fs.readdirSync(directory, {withFileTypes: true}).forEach(entry => {
+            const absolutePath = path.join(directory, entry.name);
+            if (entry.isDirectory()) {
+              addDirectory(absolutePath);
+              return;
+            }
+
+            const relativePath = path.relative(sourceRoot, absolutePath).split(path.sep).join('/');
+            const content = fs.readFileSync(absolutePath);
+            compilation.assets[relativePath] = {
+              source: () => content,
+              size: () => content.length
+            };
+          });
+        };
+
+        addDirectory(sourceRoot);
+        callback();
+      });
+    }
+  }
+
+  class CleanOutputPlugin {
+    constructor (outputDirectory) {
+      this.outputDirectory = outputDirectory;
+    }
+
+    apply (compiler) {
+      compiler.hooks.beforeRun.tap('CleanOutputPlugin', () => {
+        if (!fs.existsSync(this.outputDirectory)) {
+          return;
+        }
+
+        fs.readdirSync(this.outputDirectory).forEach(item => {
+          fs.rmSync(path.join(this.outputDirectory, item), {recursive: true, force: true});
+        });
+      });
+    }
+  }
+
   function generateHtmlPlugins (templateDir) {
     const templates = fs.readdirSync(path.resolve(__dirname, templateDir));
     return templates.map(item => {
@@ -38,11 +90,30 @@ module.exports = (env, argv) => {
   }
 
   const htmlPlugins = generateHtmlPlugins('./src/templates/page');
+  const devServer = {
+    allowedHosts: 'all',
+    port,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  };
+
+  if (serving) {
+    devServer.server = {
+      type: 'https',
+      options: {
+        key: fs.readFileSync(path.resolve(__dirname, '.cert/localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, '.cert/localhost.pem'))
+      }
+    };
+  }
 
   return {
     entry: {
       app: './src/index.js',
-      polyfill: ['babel-polyfill']
+      polyfill: ['core-js/stable', 'regenerator-runtime/runtime']
     },
     optimization: {
       splitChunks: {
@@ -58,41 +129,30 @@ module.exports = (env, argv) => {
       filename: '[name].js',
       chunkFilename: '[name].[hash].js',
       path: production ? resultPath : '/',
-      publicPath: production ? publicPath : '//localhost:' + port + '/',
+      publicPath: production ? publicPath : developmentUrl,
       futureEmitAssets: true
     },
     devtool: production ? false : 'inline-source-map',
-    devServer: {
-      disableHostCheck: true,
-      https: true,
-      port: port,
-      headers: {
-        'Access-Control-Allow-Origin': 'localhost:' + port,
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-      }
-    },
+    devServer,
     plugins: [].concat(
       new MiniCssExtractPlugin({
         filename: '[name].css',
         chunkFilename: '[id].css'
       }),
-      new CopyWebpackPlugin([{from: 'static', to: '.'}]),
+      new StaticCopyPlugin('static'),
       new VueLoaderPlugin(),
       new webpack.DefinePlugin({
-        'DIR': JSON.stringify(production ? config.dir + '/' : '//localhost:' + port + '/'),
+        'DIR': JSON.stringify(production ? config.dir + '/' : developmentUrl),
         'PRODUCTION': production,
         'VERSION': JSON.stringify(packageJson.version)
       }),
       production ? [] : htmlPlugins,
       analyze ? new BundleAnalyzerPlugin() : [],
-      production ? new CleanWebpackPlugin([resultPath + '/*'], {allowExternal: true, beforeEmit: true}) : [],
+      production ? new CleanOutputPlugin(resultPath) : [],
       production ? {
         apply: (compiler) => {
-          compiler.hooks.done.tap('Git', (compilation) => {
-            const simpleGit = require('simple-git')('../');
-            simpleGit.add('.' + publicPath + '*');
-            return true;
+          compiler.hooks.done.tap('Git', () => {
+            simpleGit('../').add('.' + publicPath + '*');
           });
         }
       } : []
@@ -138,7 +198,11 @@ module.exports = (env, argv) => {
           loader: 'postcss-loader',
           options: {
             sourceMap: production ? false : 'inline',
-            plugins: production ? [autoprefixer({remove: false}), cssnano] : []
+            postcssOptions: {
+              plugins: production
+                ? [autoprefixer({remove: false}), cssnano({preset: ['default', {zindex: false}]})]
+                : []
+            }
           }
         }, {
           loader: 'resolve-url-loader',
@@ -153,42 +217,30 @@ module.exports = (env, argv) => {
         }]
       }, {
         test: /icons.+\.svg$/,
-        use: [].concat({
-            loader: 'svg-sprite-loader',
-            options: {}
-          },
-          'svg-fill-loader',
-          production ? {
-            loader: 'image-webpack-loader'
-          } : [])
+        use: [{
+          loader: 'svg-sprite-loader',
+          options: {}
+        }]
       }, {
         test: /\.(png|jpe?g|gif)(\?.*)?$/,
-        use: [].concat({
-            loader: 'file-loader',
-            options: {
-              outputPath: 'img/',
-              name: '[name].[ext]',
-              publicPath: './img'
-            }
-          },
-          production ? [{
-            loader: 'image-webpack-loader',
-            options: {
-              disable: true
-            }
-          }] : [])
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'img/',
+            name: '[name].[ext]',
+            publicPath: './img'
+          }
+        }]
       }, {
         test: /font.+\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts/',
-              name: '[name].[ext]',
-              publicPath: './fonts'
-            }
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'fonts/',
+            name: '[name].[ext]',
+            publicPath: './fonts'
           }
-        ]
+        }]
       }]
     }
   };
